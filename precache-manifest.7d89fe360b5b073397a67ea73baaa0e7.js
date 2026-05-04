34b0543e09bd3d51b26add7d2f5193ae11b75265
Muhammad Abubakar <muhammadabubakarkhan1234@gmail.com>
2026-05-04 16:17:16 +0500

    Updates

---
+++ precache-manifest.7d89fe360b5b073397a67ea73baaa0e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af496a464d4ccc8fcdc96ac483846a16",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "87bbb017aa407f0d0117",
+    "url": "/My-Portfolio/static/css/2.032362bf.chunk.css"
+  },
+  {
+    "revision": "edb275f1752fb0ee3759",
+    "url": "/My-Portfolio/static/css/main.58eaa137.chunk.css"
+  },
+  {
+    "revision": "87bbb017aa407f0d0117",
+    "url": "/My-Portfolio/static/js/2.188a8bf3.chunk.js"
+  },
+  {
+    "revision": "edb275f1752fb0ee3759",
+    "url": "/My-Portfolio/static/js/main.7d2b08f2.chunk.js"
+  },
+  {
+    "revision": "63b51f4e795b1e74312e",
+    "url": "/My-Portfolio/static/js/runtime~main.da10889d.js"
+  },
+  {
+    "revision": "2574f75ca44b0010e0c701b0656e7dca",
+    "url": "/My-Portfolio/static/media/Love-Coding.2574f75c.webm"
+  },
+  {
+    "revision": "5683999a71bbd66f022522acb9f56cca",
+    "url": "/My-Portfolio/static/media/Love-Coding.5683999a.mp4"
+  },
+  {
+    "revision": "16d35bdd549bb35e76c6517648dcd8db",
+    "url": "/My-Portfolio/static/media/abubakar.16d35bdd.jpeg"
+  }
+]);
